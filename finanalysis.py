d0394f5b01018188dda884ae52786c15fe879870
Chris S. <8145582+csanto931@users.noreply.github.com>
2025-07-23 22:17:20 -0700

    Update finanalysis.py

--- finanalysis.py
+++ finanalysis.py
@@ -9,7 +9,7 @@ def portfolio_optimization(returns):
 def fetch_data(tickers, start_date, end_date):
     data_frames = []
     for ticker in tickers:
-        stock_data = yf.download(ticker, start=start_date, end=end_date)['Adj Close']
+        stock_data = yf.download(ticker, start=start_date, end=end_date)['Close']
         data_frames.append(stock_data)
     
     data = pd.concat(data_frames, axis=1, keys=tickers)
